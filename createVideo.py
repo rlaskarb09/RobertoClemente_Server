80d4316acdfe59e3a7c94bb55d5746d66400f299
chwaaaa <59624707+chwaaaa@users.noreply.github.com>
2020-01-09 14:37:20 -0500

    Add files via upload
    create video from images

---
+++ createVideo.py
@@ -0,0 +1,13 @@
+
+import cv2
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
+out = cv2.VideoWriter('../demo20.mp4', fourcc, 20.0, (480,360), True)
+
+for i in range(604, 1722):
+    filename ='../Img_%d.jpg'%i
+    img = cv2.imread(filename)
+    out.write(img)
+
+out.release()
+cv2.destroyAllWindows()
