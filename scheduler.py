7f302f85bc1fc974804b05f9790fd8fa0f6ea5c0
sunshinej01 <59624777+sunshinej01@users.noreply.github.com>
2020-01-23 15:19:58 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -37,30 +37,30 @@ def SmallandAdd(Q, Dlist):
     for i in range(orderlist.shape[0]):
         idx = np.where(orderlist[:,1]==orderlist[i,1])[0]
         if idx.shape[0] != 1 and i==idx[0]:
-            #dist = distance(orderlist[idx][2], curr) # 이전주소와 거리계산
-            #dist sort -> index 받아서 orderlist[idx]로
-            addsort = sorted(orderlist[idx], key=lambda x: x[2])
-            addsort = np.asarray(addsort)
-            orderlist[idx] = addsort
+            if i != 0:
+                curr = orderlist[i - 1][2]
+            sortedidx = distance(orderlist[idx,2], curr)
+            orderlist[idx] = orderlist[idx[sortedidx]]
     for n in range(orderlist.shape[0]):
         Dlist.append(orderlist[n][0])
     return Dlist
 
-def distance(adds, curr): # 얘를 수정
-    a = 3
-    b = 2
-    c = 2
-    d = [a, b, b, a, c, c] # 201-101-102-103-203-202-201
-    dist = 0
-    for i in range(adds.shape[0]):
+def distance(adds, curr):
+    d = [3, 2, 2, 3, 2, 2] # 201-101-102-103-203-202-201
+    size = (adds.shape[0])
+    ddd = np.zeros(size)
+    for i in range(size):
+        dist = 0
         add = adds[i]
         if curr != add: # next address != previous address
             if curr == -1:
-                dist = np.sum(d[:add-1])
+                dist = np.sum(d[:add-1])-d[0]/2
             else:
                 if curr < add:
                     dist = np.sum(d[curr:add-1])
                 else:
                     dist = np.sum(d[curr:]) + np.sum(d[:add-1])
-    return dist
+        ddd[i] = dist
+    sortedidx = np.argsort(ddd)
+    return sortedidx
 
