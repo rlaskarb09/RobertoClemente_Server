75c5ddccb0ac06ab43c95a466c23741b4b135297
Namkyu Kim <rlaskarb09@naver.com>
2020-01-31 20:42:07 -0500

    add pendingItems-2

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ let ejs = require('ejs');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ehfpalvkthf',
+    password: '',
     database: 'orderdb',
     debug: false
 });
