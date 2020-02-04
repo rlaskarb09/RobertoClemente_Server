9d92cebb2cf49696b58f8a25e3c13c962e597a0a
sunshinej01 <59624777+sunshinej01@users.noreply.github.com>
2020-02-04 14:52:04 -0500

    Update scheduler.py

--- simulator/scheduler.py
+++ simulator/scheduler.py
@@ -11,16 +11,23 @@ def FIFO(Q, Dlist):
     return Dlist # delivery list
 
 # item 적은순
-def smallorder(Q, Dlist):
-    orderlist = []
+def smallorder(Q, orderlist):
+    Dlist = []
     while Q.empty() == False:
         order = Q.get()
-        orderlist.append((order, order._N))
-    orderlist = sorted(orderlist, key = lambda x: x[1])
-    for (order, n) in orderlist:
+        orderlist.append([order, order._N, order._filldate])
+    k = 0
+    for (order, _, filldate) in orderlist:
+        k += 1
+        if filldate != None:
+            break
+    if k == len(orderlist):
+        k = 0
+    orderlist = sorted(orderlist[k:], key = lambda x: x[1])
+    for (order, n, _) in orderlist:
         for i in range(n):
             Dlist.append(order)
-    return Dlist
+    return Dlist, orderlist
 
 # item수 + address
 def SmallandAdd(Q, Dlist):
@@ -67,49 +74,17 @@ def distance(adds, curr):
     sortedidx = np.argsort(ddd)
     return sortedidx
 
-def SmallandAdd2(Q, Dlist, maxitem):
-    orderlist = []
-    dict = {101: 1, 102: 2, 103: 3, 203: 4, 202: 5, 201: 6} # order._address = key
-    curr = -1
-    pcknum = np.zeros(Q.qsize())
-    while Q.empty() == False:
-        order = Q.get()
-        orderlist.append((order, order._N, dict[order._address]))
-    # sort by N of items
-    orderlist = sorted(orderlist, key=lambda x: x[1])
-    orderlist = np.asarray(orderlist)
-    # sort by address if N of items is same
-    for i in range(orderlist.shape[0]):
-        idx = np.where(orderlist[:,1]==orderlist[i,1])[0]
-        if idx.shape[0] != 1 and i==idx[0]:
-            if i != 0:
-                curr = orderlist[i - 1][2]
-            sortedidx = distance(orderlist[idx,2], curr)
-            orderlist[idx] = orderlist[idx[sortedidx]]
-    for i in range(len(orderlist)):
-        pcknum[i] = int(np.sum(orderlist[:i+1,1])/maxitem)
-    for i in range(int(pcknum[-1])):
-        pck = np.where(pcknum==i)[0]
-        if pck.shape[0] != 1:
-            idx = np.argsort(np.asarray(orderlist[pck,2]))
-            orderlist[pck]=orderlist[idx]
-    '''for n in range(orderlist.shape[0]):
-        Dlist.append(orderlist[n][0])'''
-    for (order, n, _) in orderlist:
-        for i in range(n):
-            Dlist.append(order)
-    return Dlist
-
 def undelivered_orders(orderlist):
     i = 0
-    for (order, _, _, filldate) in orderlist:
+    for (_, _, _, filldate) in orderlist:
         i += 1
         if filldate != None:
             break
     return i
 
+# scheduling available with updated orders
 def updatescedule(Q, maxitem, orderlist):
-    dict = {101: 1, 102: 2, 103: 3, 203: 4, 202: 5, 201: 6} # order._address = key
+    dict = {201: 1, 202: 2, 203: 3, 103: 4, 102: 5, 101: 6} # order._address = key
     curr = -1
     Dlist = []
     while Q.empty() == False:
@@ -120,7 +95,7 @@ def updatescedule(Q, maxitem, orderlist):
     if k == len(orderlist):
         k = 0
     orderlist = sorted(orderlist[k:], key=lambda x: x[1])
-    pcknum = np.zeros(len(orderlist))
+    #pcknum = np.zeros(len(orderlist))
     orderlist = np.asarray(orderlist)
     # sort by address if N of items is same
     for i in range(orderlist.shape[0]):
@@ -133,7 +108,7 @@ def updatescedule(Q, maxitem, orderlist):
             temp2 = temp[sortedidx]
             orderlist[idx] = temp2
             #orderlist[idx] = orderlist[idx[sortedidx]]
-    for i in range(len(orderlist)):
+    '''for i in range(len(orderlist)):
         pcknum[i] = int(np.sum(orderlist[:i+1,1])/maxitem)
     for i in range(int(pcknum[-1])):
         pck = np.where(pcknum==i)[0]
@@ -142,11 +117,114 @@ def updatescedule(Q, maxitem, orderlist):
             temp = orderlist[pck]
             temp2 = temp[idx]
             orderlist[pck] = temp2
-            #orderlist[pck] = orderlist[idx]
-    for (order, n, _, _) in orderlist:
+            #orderlist[pck] = orderlist[idx]'''
+    for (order, n, addidx, _) in orderlist:
         for i in range(n):
-            Dlist.append(order)
-    return Dlist, orderlist.tolist()
+            Dlist.append([order, addidx])
+    cycle = int(len(Dlist) / maxitem)
+    for i in range(cycle):
+        p = Dlist[i * maxitem:(i + 1) * maxitem]
+        p2 = sorted(p, key=lambda x: x[1])
+        Dlist[i * maxitem:(i + 1) * maxitem] = p2
+    Dlist = np.asarray(Dlist)[:,0]
+    return Dlist.tolist(), orderlist.tolist()
+
+# backward movement added
+def UpdateandBack(Q, maxitem, orderlist):
+    dict = {201: 1, 202: 2, 203: 3, 103: 4, 102: 5, 101: 6} # order._address = key
+    curr = -1
+    Dlist = []
+    back = 0 # go forward when 0, turn 180 after this when 1, turn 180 before this when -1
+    while Q.empty() == False:
+        order = Q.get()
+        orderlist.append([order, order._N, dict[order._address], order._filldate])
+    # sort by N of items
+    k = undelivered_orders(orderlist)
+    if k == len(orderlist):
+        k = 0
+    orderlist = sorted(orderlist[k:], key=lambda x: x[1])
+    #pcknum = np.zeros(len(orderlist))
+    orderlist = np.asarray(orderlist)
+    # sort by address if N of items is same
+    for i in range(orderlist.shape[0]):
+        idx = np.where(orderlist[:,1]==orderlist[i,1])[0]
+        if idx.shape[0] != 1 and i==idx[0]:
+            if i != 0:
+                curr = orderlist[i - 1][2]
+            sortedidx = distance(orderlist[idx,2], curr)
+            temp = orderlist[idx]
+            temp2 = temp[sortedidx]
+            orderlist[idx] = temp2
+            #orderlist[idx] = orderlist[idx[sortedidx]]
+    '''for i in range(len(orderlist)):
+        pcknum[i] = int(np.sum(orderlist[:i+1,1])/maxitem)
+    for i in range(int(pcknum[-1])):
+        pck = np.where(pcknum==i)[0]
+        if pck.shape[0] != 1:
+            idx = np.argsort(np.asarray(orderlist[pck,2]))
+            temp = orderlist[pck]
+            temp2 = temp[idx]
+            orderlist[pck] = temp2'''
+    for (order, n, addidx, _) in orderlist:
+        for i in range(n):
+            Dlist.append([order, back, addidx])
+    # backward condition
+    cycle = int(len(Dlist)/maxitem)
+    for i in range(cycle):
+        p = Dlist[i*maxitem:(i+1)*maxitem]
+        p2 = sorted(p, key=lambda x: x[1])
+        if p2[-1][2] < 4:
+            p2[-1][1] = 1 # turn 180 degree after this delivery
+            Dlist[(i+1)*maxitem][1] = -1
+        elif p2[0][2] > 3:
+            p2[0][1] = -1  # turn 180 degree before going this address
+            p2[-1][1] = 1 # turn 180 after delivering this address
+        Dlist[i*maxitem:(i+1)*maxitem] = p2
+    Dlist0 = np.asarray(Dlist)[:,0]
+    Dlist1 = np.asarray(Dlist)[:,1]
+    Dlist = np.vstack((Dlist0, Dlist1)).transpose()
+    return Dlist.tolist(), orderlist.tolist()
 
 
-# 중간에 update된 order/deliver 완료된 order 구분해서 다시 scheduling 가능하게!!
+def BackSimple(Q, maxitem, orderlist):
+    dict = {201: 1, 202: 2, 203: 3, 103: 4, 102: 5, 101: 6} # order._address = key
+    curr = -1
+    Dlist = []
+    back = 0 # go forward when 0, go backward after this when 1
+    while Q.empty() == False:
+        order = Q.get()
+        orderlist.append([order, order._N, dict[order._address], order._filldate])
+    # sort by N of items
+    k = undelivered_orders(orderlist)
+    if k == len(orderlist):
+        k = 0
+    orderlist = sorted(orderlist[k:], key=lambda x: x[1])
+    orderlist = np.asarray(orderlist)
+    # sort by address if N of items is same
+    for i in range(orderlist.shape[0]):
+        idx = np.where(orderlist[:,1]==orderlist[i,1])[0]
+        if idx.shape[0] != 1 and i==idx[0]:
+            if i != 0:
+                curr = orderlist[i - 1][2]
+            sortedidx = distance(orderlist[idx,2], curr)
+            temp = orderlist[idx]
+            temp2 = temp[sortedidx]
+            orderlist[idx] = temp2
+    for (order, n, addidx, _) in orderlist:
+        for i in range(n):
+            Dlist.append([order, back, addidx])
+    # backward condition
+    cycle = int(len(Dlist)/maxitem)
+    for i in range(cycle):
+        p = Dlist[i*maxitem:(i+1)*maxitem]
+        p2 = sorted(p, key=lambda x: x[1])
+        if p2[-1][2] == 1: # all the order address = 201
+            p2[-1][1] = 1 # turn 180 degree after this delivery
+            #Dlist[(i+1)*maxitem][1] = -1
+        elif p2[0][2] == 6: # all the order address = 101
+            p2[0][1] = 1  # turn 180 after delivering this address
+        Dlist[i*maxitem:(i+1)*maxitem] = p2
+    Dlist0 = np.asarray(Dlist)[:,0]
+    Dlist1 = np.asarray(Dlist)[:,1]
+    Dlist = np.vstack((Dlist0, Dlist1)).transpose()
+    return Dlist.tolist(), orderlist.tolist()
